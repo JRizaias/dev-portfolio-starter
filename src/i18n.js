be09b23aff7b9d04453a73647ea0eca1d9576a3d
Izaias Junior <izaiasjunior747@gmail.com>
2025-07-29 00:23:11 -0300

    chore(i18n): add i18n.js configuration file for i18next integration

---
+++ src/i18n.js
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import pt from './data/pt.json';
+import en from './data/en.json';
+
+const resources = {
+  pt: { translation: pt },
+  en: { translation: en },
+};
+
+i18n
+  .use(LanguageDetector)
+  .init({
+    resources,
+    fallbackLng: 'pt',
+    debug: false,
+    interpolation: { escapeValue: false },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
